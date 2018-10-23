bca49f6c6d8d9856641eb3362449dfcbe35cfded
Jefferson Silva <34176051+Jefferson023@users.noreply.github.com>
2018-10-23 19:49:06 -0300

    Tweets quase feito

--- tweets/process.py
+++ tweets/process.py
@@ -1,9 +1,9 @@
 from contextlib import redirect_stdout
 from re import compile, sub
 
-regular = compile(r"RT (@\S+):.*(http\S+)")
+regular = compile(r"RT (@\S+):.*(https://t\S+)")
 with open("tweets.in", encoding="utf-8") as f:
-    tweets = [user+ '\t' + sub('…', '', link) for user, link in regular.findall(f.read())]
+    tweets = [user+ '\t' + sub('…', '', link) if len(link) > 11 else user+ '\t' + sub('.…', '', link) for user, link in regular.findall(f.read())]
     tweets.sort()
 
 with open("tweets.out", "w", encoding="utf-8") as f:
